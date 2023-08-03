1877423d73a58868971dc775f9ed4c2a57aa24d6
agustinguillen <agustinnguillen@gmail.com>
2023-08-03 12:21:07 -0300

    desktop mockups finished

--- app/footer.tsx
+++ app/footer.tsx
@@ -8,40 +8,33 @@ import { usePathname } from 'next/navigation';
 export default function Footer() {
   const pathname = usePathname();
   return (
-    <div className={`bg-[#f8f8f8] ${pathname === '/' ? 'footer-custom' : ''}`}>
-      <section className={'container flex flex-wrap justify-between gap-10 px-3 py-10 mt-32'}>
-        <div className='space-y-4'>
-          <Brand pathname={pathname} />
-          <p className='text-neutral-800 text-[14px] font-normal leading-snug'>
-          Welcome to the first marketplace that allows you 
-            <br />
-            to safely and securely invest in content creators 
-            <br />
-            through tokens and direct funding solutions.{' '}
-          </p>
-          <div className='flex gap-2'>
-            {Array.from(Array(4).keys()).map((_) => (
-              <Link href={'/'} key={_}>
-                <Image
-                  src={`/assets/images/${_ + 1}.svg`}
-                  alt='   '
-                  width={26}
-                  height={26}
-                />
-              </Link>
-            ))}
-          </div>
-        </div>
+    <div className={`bg-[#f8f8f8] py-24 ${pathname === '/' ? 'footer-custom mt-36' : ''}`}>
+      <div className='w-full flex justify-center pb-0'>
+        <Brand pathname={pathname} imageSize={60} textSize={'40px'} />
+      </div>
+      <div className='w-full flex justify-center gap-2 p-6'>
+        {Array.from(Array(4).keys()).map((_) => (
+          <Link href={'/'} key={_}>
+            <Image
+              src={`/assets/images/${_ + 1}.svg`}
+              alt='   '
+              width={80}
+              height={80}
+            />
+          </Link>
+        ))}
+      </div>
+      <section className={'container flex flex-wrap justify-between gap-10 px-3'}>
         {/* middle part */}
-        <div className='flex flex-wrap gap-12 '>
+        <div className='w-full flex flex-wrap'>
           {data.map((_) => (
-            <div key={_.title}>
-              <p className=' pb-1 text-neutral-800 text-[15px] font-bold capitalize leading-relaxed'>
+            <div key={_.title} className='w-[25%]'>
+              <p className={`pb-1 ${pathname === '/' ? 'text-white' : 'text-neutral-800'} text-[20px] font-bold capitalize leading-relaxed`}>
                 {_.title}
               </p>
               {_.sub.map((__) => (
                 <Link
-                  className='text-neutral-800 py-1.5 block text-[14px] font-normal leading-snug'
+                  className={`${pathname === '/' ? 'text-neutral-500' : 'text-neutral-800'} py-1.5 block text-[14px] font-normal leading-snug capitalize`}
                   href={__}
                   key={__}
                 >
@@ -50,40 +43,30 @@ export default function Footer() {
               ))}
             </div>
           ))}
-        </div>
-        <div className='max-w-xs'>
-          <p className=' pb-1 text-neutral-800 text-[15px] font-bold capitalize leading-relaxed'>
-            Subscribe
-          </p>{' '}
-          <Link
-            className='text-neutral-800 py-1.5 block text-[14px] font-normal leading-snug'
-            href={'/'}
-          >
-            Signup for our newsletter to get the latest news in your inbox.
-          </Link>
-          <form className='flex items-stretch overflow-hidden border rounded-lg border-black/5 w-fit '>
-            <input
-              type='email'
-              placeholder='Enter your email'
-              className='h-8 pl-3 outline-none max-[280px]:w-36'
-            />
-            <button className='flex h-full px-2 bg-secondary '>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                fill='none'
-                viewBox='0 0 24 24'
-                stroke-width='1.5'
-                stroke='currentColor'
-                className='w-6 h-8 fill-white stroke-secondary bg-secondary'
-              >
-                <path
-                  stroke-linecap='round'
-                  strokeLinejoin='round'
-                  d='M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5'
+          <div className='w-[25%]'>
+            <p className={`pb-1 ${pathname === '/' ? 'text-white' : 'text-neutral-800'} text-[20px] font-bold capitalize leading-relaxed`}>
+              Subscribe
+            </p>{' '}
+            <Link
+              className={`${pathname === '/' ? 'text-neutral-500' : 'text-neutral-800'} py-1.5 block text-[14px] font-normal leading-snug mb-6`}
+              href={'/'}
+            >
+              Signup for our newsletter to get the latest news in your inbox.
+            </Link>
+            <form className='flex items-stretch overflow-hidden rounded-lg w-full '>
+              <div className="relative flex items-center w-full">
+                <input
+                  type='email'
+                  placeholder='Enter your email'
+                  className={`h-12 pl-3 outline-none w-full border border-white rounded-full ${pathname === '/' ? 'bg-white' : 'bg-black'} bg-opacity-20`}
                 />
-              </svg>
-            </button>
-          </form>
+                <button className='flex items-center justify-center h-9 px-2 bg-secondary rounded-full absolute right-2 text-white'>
+                  SEND
+                </button>
+              </div>
+            </form>
+
+          </div>
         </div>
       </section>
     </div>
@@ -92,8 +75,8 @@ export default function Footer() {
 
 const data = [
   {
-    title: 'Marketplace ',
-    sub: ['All Tokens', "Trending", 'Gaming', 'Lifestyle', 'Comedy' , "More"],
+    title: 'Resources',
+    sub: ['Help & Support', "Terms & Services", 'Security', 'Privacy Policy'],
   },
   {
     title: 'Company ',
@@ -101,8 +84,8 @@ const data = [
   },
   {
     title: 'Learn / FAQS',
-    sub: ['Blockchain', 'Tokens', 'Creator Tokens', 'FAQs'],
+    sub: ['What are creator tokens ?', 'How to buy creator tokens?', 'How to create yout own token?', 'What does “minting” mean?', 'How do i sell my token?', 'how to withdrawal funds?', 'What is a crypto wallet?', 'Introduction to blockcahin', 'What are cryptocurrencies?'],
   },
-  
-  
+
+
 ];
--- components/ui/brand.tsx
+++ components/ui/brand.tsx
@@ -1,16 +1,22 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-export default function Brand({pathname}:{pathname: string}) {
+export default function Brand(
+  {pathname, imageSize, textSize}
+  :{
+    pathname: string
+    imageSize?: number
+    textSize?: string | number
+  }) {
   return (
     <Link href={'/'} className={`flex items-center gap-3 ${pathname === '/' ? 'text-white' : ''}`}>
       <Image
         src={'/assets/images/logo.svg'}
         alt='logo'
-        width={35}
-        height={35}
+        width={imageSize ? imageSize : 35}
+        height={imageSize ? imageSize : 35}
       />
-      <p className='text-xl font-extrabold'>Innox</p>
+      <p className='text-xl font-extrabold' style={{ fontSize: textSize }}>Innox</p>
     </Link>
   );
 }
